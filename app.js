24f25f1ea3151a29fcce6fda865f7f3cf3f1f4c1
ramzi-abulai <ramziabulail84@gmail.com>
2023-10-03 23:10:42 +0300

    re

--- app.js
+++ app.js
@@ -1,15 +1,54 @@
-let theName = window.prompt("Enter you name", "");
-let gender = window.prompt("your gender", "");
+var theName;
+var gender;
 
-if (gender == "male"){
+var male = "MALE";
+var female = "FEMALE";
+
+let A = ["Item 1", "Item 2", "Item 3", "Item 4"];
+
+
+theName = window.prompt("Enter Your Name", "");
+A[0] = theName;
+
+
+gender = window.prompt("Enter Your Gender", "").toUpperCase();
+
+
+function LGender() {
+    while (true) {
+        if (gender !== male && gender !== female) {
+            gender = window.prompt("Enter Your Gender", "").toUpperCase();
+            A[1] = gender;
+        } else {
+            break;
+            
+        }
+    }
+}
+
+LGender();
+
+if (gender == "MALE"){
     window.alert("Welcome Mr " + theName);
-} else if (gender == "female"){
+} else if (gender == "FEMALE"){
     window.alert("Welcome Ms "+ theName)
 } else
     window.alert("Welcome " + theName);
 
+
+
+
 let hotCold = window.prompt("do you want it hot or cold", "");
+A[2] = hotCold;
+
 let drink = window.prompt("what is your drink", "");
+A[3] = drink;
+
+
 let prepared = window.alert("Your Drink is being Prepared");
 
 console.log("Name : " +  theName +'\n'  +"Drink : " + drink);
+
+for(x = 0 ;x <= A.length -1; x++){
+    console.log(A[x]);
+}
